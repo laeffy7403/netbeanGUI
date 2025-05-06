1670e30868964dd8a8908e51e00895152637b77e
ThingWei <chngthingwei@gmail.com>
2025-05-06 18:46:37 +0800

    test

--- src/java/controller/Login.java
+++ src/java/controller/Login.java
@@ -1,85 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package controller;
 
+
+import model.Customer;
+
+import model.UserDAO;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author chinw
- */
 public class Login extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Login</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UserDAO dao = new UserDAO();
+        HttpSession session = request.getSession();
+
+        if (dao.validateCustomer(username, password)) {
+            Customer user = dao.getCustomerByUsername(username);
+            session.setAttribute("user", user);
+            session.setAttribute("role", "customer");
+            session.setAttribute("username", username);
+            response.sendRedirect("ProfileController");
+        } else if (dao.validateStaff(username, password)) {
+            session.setAttribute("role", "staff");
+            session.setAttribute("username", username);
+            response.sendRedirect("pages/user/staff_dashboard.jsp");
+        } else if (dao.validateAdmin(username, password)) {
+            session.setAttribute("role", "admin");
+            session.setAttribute("username", username);
+            response.sendRedirect("pages/user/admin_dashboard.jsp");
+        } else {
+            request.setAttribute("error", "Invalid credentials");
+            request.getRequestDispatcher("loginError.html").forward(request, response);
+        }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
