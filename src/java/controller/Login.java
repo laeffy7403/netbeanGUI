e94916868522d4af464b46b64155b9f48f519422
LigitPizza <laeffy7403@gmail.com>
2025-05-09 16:16:03 +0800

    change path in login.java

--- src/java/controller/Login.java
+++ src/java/controller/Login.java
@@ -25,15 +25,15 @@ public class Login extends HttpServlet {
             session.setAttribute("user", user);
             session.setAttribute("role", "customer");
             session.setAttribute("username", username);
-            response.sendRedirect("ProfileController");
+            response.sendRedirect("pages/homepage.jsp");
         } else if (dao.validateStaff(username, password)) {
             session.setAttribute("role", "staff");
             session.setAttribute("username", username);
-            response.sendRedirect("pages/user/staff_dashboard.jsp");
+            response.sendRedirect("staff_dashboard.jsp");
         } else if (dao.validateAdmin(username, password)) {
             session.setAttribute("role", "admin");
             session.setAttribute("username", username);
-            response.sendRedirect("pages/user/admin_dashboard.jsp");
+            response.sendRedirect("admin_dashboard.jsp");
         } else {
             request.setAttribute("error", "Invalid credentials");
             request.getRequestDispatcher("loginError.html").forward(request, response);
